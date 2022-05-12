d693eea6d5a933a555e937a7bf877590699dc2bf
Axel Suarez Martinez <axsuarez@microsoft.com>
2022-05-11 18:15:18 -0700

    Adding generic flattening

--- src/typedStructuredCondition.ts
+++ src/typedStructuredCondition.ts
@@ -10,12 +10,60 @@ function typedStructuredCondition(expression: string): BooleanConditionBase {
     return castingTraverse(structuredConditionTree);
 }
 
-function castingTraverse(parseTree): BooleanConditionBase {
+function extractChildrenNodes(treeNode){
+    const result = [];
+    if (treeNode.child){
+        result.push(treeNode.child)
+        delete treeNode.child
+    } else{
+        if (treeNode.left){
+            result.push(treeNode.left)
+            delete treeNode.left
+        }
+        if (treeNode.right){
+            result.push(treeNode.right)
+            delete treeNode.right
+        }
+    }
+    return result
+}
 
+function binaryToNary(bNode, exclusiveOperatorsToFlatten: string[] = null) {
+    if (!bNode) return null;
+    let leaves = extractChildrenNodes(bNode);
+    bNode.children = []
+    leaves.forEach(element => {
+        if(!element) return;
+        var flatNode = binaryToNary(element);
+        if (operatorsShouldBeFlatten(element.token_type, bNode.token_type, exclusiveOperatorsToFlatten)){
+            bNode.children.push(...flatNode.children);
+        }
+        else {
+            bNode.children.push(flatNode);
+        }
+    });
+    return bNode;
+}
+
+function operatorsShouldBeFlatten(current_token_type: any, child_token_type: any, exclusiveOperatorsToFlatten: string[]): boolean {
+    if (current_token_type != child_token_type) {
+        return false;
+    }
+    else if(!exclusiveOperatorsToFlatten || exclusiveOperatorsToFlatten.length == 0) {
+        return true;
+    }
+
+    return exclusiveOperatorsToFlatten.includes(current_token_type);
+}
+
+
+
+function castingTraverse(parseTree): BooleanConditionBase {
     return null
 }
 
 
 export{
-    typedStructuredCondition
+    typedStructuredCondition,
+    binaryToNary
 }
